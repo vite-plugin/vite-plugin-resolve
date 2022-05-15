0774ea8560f8feb90e0a03036642c05ba6caf0fc
younglei <youngleish@gmail.com>
2022-05-15 17:27:34 +0800

    feat: add preset files

---
+++ presets/redux-v5.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import('.').LibMeta}
+ */
+ module.exports = {
+  name: 'Redux',
+  members: [
+    '__DO_NOT_USE__ActionTypes',
+    'applyMiddleware',
+    'bindActionCreators',
+    'combineReducers',
+    'compose',
+    'createStore',
+  ],
+};
