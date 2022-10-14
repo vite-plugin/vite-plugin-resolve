03543b4e3bb8fa09cc0f21c4da7aae4c289feaca
草鞋没号 <308487730@qq.com>
2022-10-14 10:13:11 +0800

    feat: support `type: "module"` 🌱

--- presets/index.d.ts
+++ presets/index.d.ts
@@ -1,3 +1,5 @@
+export * from './libs'
+
 export interface LibMeta {
   name: string
   members: string[]
@@ -22,66 +24,3 @@ export interface Lib2esm {
   (name: string, members: string[], options?: Lib2esmOptions): string
 }
 export declare const lib2esm: Lib2esm
-
-export declare const antd_vue: {
-  v1: string
-  v3: string
-}
-
-export declare const antd: {
-  v4: string
-}
-
-export declare const element_plus: {
-  v2: string
-}
-
-export declare const element_ui: {
-  v2: string
-}
-
-export declare const pinia: {
-  v2: string
-}
-
-export declare const react_dom: {
-  v17: string
-  v18: string
-}
-
-export declare const react_router_dom: {
-  v5: string
-  v6: string
-}
-
-export declare const react_router: {
-  v5: string
-  v6: string
-}
-
-export declare const react: {
-  v17: string
-  v18: string
-}
-
-export declare const redux: {
-  v5: string
-}
-
-export declare const vue_composition_api: {
-  v1: string
-}
-
-export declare const vue_router: {
-  v4: string
-}
-
-export declare const vue: {
-  v2: string
-  v3: string
-}
-
-export declare const vuex: {
-  v3: string
-  v4: string
-}
