fa0313f4a74aeaed08009426200c227074177383
草鞋没号 <308487730@qq.com>
2022-10-19 15:17:19 +0800

    refactor(2.3.0): remove `lib2esm()`

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,9 @@
-import type { Plugin } from 'vite';
-
-export const lib2esm: import('./presets').Lib2esm;
-
 declare module 'vite-plugin-resolve' {
   function resolve(entries: {
     [moduleId: string]:
     | import('rollup').LoadResult
-    | Plugin['load'];
-  }): Plugin[];
+    | import('vite').Plugin['load'];
+  }): import('vite').Plugin[];
 
   // https://www.typescriptlang.org/docs/handbook/declaration-files/templates/module-d-ts.html#default-exports
   export = resolve;
--- presets/index.js
+++ presets/index.js
@@ -1,37 +1,12 @@
 const libEsm = require('lib-esm');
 
-/**
- * @type {import('.').Lib2esm} 
- */
-exports.lib2esm = function (name, ...args) {
-  if (!args.length) {
-    return exports.lib2esm(name, [], {});
-  }
-  if (args.length === 1) {
-    return Object.prototype.toString.call(args[0]) === '[object Object]'
-      // lib2esm(name, options)
-      ? exports.lib2esm(name, [], args[0])
-      // lib2esm(name, members)
-      : exports.lib2esm(name, args[0], {})
-  }
-  const [members, options] = args;
-  const { format = 'iife' } = options;
-
-  const result = libEsm({
-    require: name,
-    exports: members,
-    format,
-  })
-
-  return `${result.require}\n${result.exports}`;
-};
-
 for (const [name, versions] of Object.entries(require('./libs.json'))) {
   const record = {};
   for (const [version, file] of Object.entries(versions)) {
     /** @type {import('.').LibMeta} */
     const lib = require(`./${file}`);
-    record[version] = this.lib2esm(lib.name, lib.members);
+    const result = libEsm({ window: lib.name, exports: Object.keys(lib) });
+    record[version] = `${result.window}\n${result.exports}`;
   }
   exports[name.replaceAll('-', '_')] = record;
 }
