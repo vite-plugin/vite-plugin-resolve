547aba1d37868a4b99532fa94a1d7a73b8d53788
野鸡没名 <asd308487730@gmail.com>
2023-08-21 15:26:12 +0800

    fix(preset): export `lib.members`

--- presets/index.js
+++ presets/index.js
@@ -5,7 +5,7 @@ for (const [name, versions] of Object.entries(require('./libs.json'))) {
   for (const [version, file] of Object.entries(versions)) {
     /** @type {import('.').LibMeta} */
     const lib = require(`./${file}`);
-    const result = libEsm({ window: lib.name, exports: Object.keys(lib) });
+    const result = libEsm({ window: lib.name, exports: lib.members });
     record[version] = `${result.window}\n${result.exports}`;
   }
   exports[name.replaceAll('-', '_')] = record;
